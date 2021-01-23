f3dca05cdca30b719711106e1413fb482c38c4a5
barisx <barissenyerli@gmail.com>
2021-01-24 00:51:32 +0300

    Jan 24: Observer end

---
+++ 2-abstract_factory_example.py
@@ -0,0 +1,42 @@
+class Dog:
+    """A simple dog class"""
+    def speak(self):
+        return "Woof"
+
+    def __str__(self):
+        return "Dog"
+
+class DogFactory:
+    """Concrete Factory"""
+
+    def get_pet(self):
+        """Returns a Dog object"""
+        return Dog()
+    def get_food(self):
+        """Returns a Dof Food object"""
+        return "Dog Food!"
+
+class PetStore:
+    """ PetStore houses our Abstract Factory """
+
+    def __init__(self, pet_factory=None):
+        """ pet_factory is our Abstract Factory """
+        self._pet_factory = pet_factory
+
+    def show_pet(self):
+        """ Utility method to display the details the objects returned by the
+        DogFactory"""
+
+        pet = self._pet_factory.get_pet()
+        pet_food = self._pet_factory.get_food()
+
+        print("Our pet is '{}'".format(pet))
+        print("Our pet says hello by '{}'".format(pet.speak()))
+        print("Its food is '{}'".format(pet_food))
+
+factory = DogFactory()
+
+shop = PetStore(factory)
+
+shop.show_pet()
+
