8bfa3f5e4154f4ba6061ee8b8a650522346519ba
barisx <baris.senyerli@akinon.com>
2021-02-15 23:11:50 +0300

    Feb 15: TDD Course end

---
+++ Unit Testing and Test Driven Development on Python/Checkout/TestCheckout.py
@@ -0,0 +1,42 @@
+import pytest
+from Checkout import Checkout
+
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    return checkout
+
+# def test_CanInstantiateCheckout():
+#     co = Checkout()
+
+# def test_CanAddItemPrice(checkout):
+#     checkout.addItemPrice("a", 1)
+#
+# def test_CanAddItem(checkout):
+#     checkout.addItem("a", 1)
+
+def test_CanCalculateTotal(checkout):
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
+
+def test_canAddDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
+
+#@pytest.mark.skip
+def test_canApplyDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
+
+def test_ExceptionWithBadItem(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
