4c62e1e1c1bcd13f91a011e2841e4b5e7780ec4d
barisx <baris.senyerli@akinon.com>
2021-03-02 11:36:20 +0300

    Mar 2: add docstring, defdict, namedtup

---
+++ Advance Python/9-namedtup_start.py
@@ -0,0 +1,23 @@
+# Demonstrate the usage if namedtuple objects
+
+import collections 
+
+
+def main():
+    # TODO: create a Point namedtuple
+    Point = collections.namedtuple("Point", "x y")
+    p1 = Point(10, 20)
+    p2 = Point(30, 40)
+    print(p1, p2)
+    # Point(x=10, y=20), Point(x=30, y=40))
+    print(p1.x, p2.y)
+    # (10, 40)
+
+    # TODO: use _replace to create a new instance
+    p1 = p1._replace(x=100)
+    print(p1)
+    # Point(x=100, y=20)
+
+
+if __name__ == "__main__":
+    main()
